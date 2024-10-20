604c4fb0a92a79b9b92a719492d14887e03ac7cc
Artem <69763892+artem9677@users.noreply.github.com>
2024-10-20 17:27:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-string = str(input("Введите строку: "))
+string = str(input("Введите строку: ")) # ввод строки
 
-for i in range(len(string)):
+for i in range(len(string)): # проверка и вывод индексов
     if string[i]=="е":
         print(i)
